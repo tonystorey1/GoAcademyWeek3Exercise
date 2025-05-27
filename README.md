294963a357630dabb233b2242886b472ab807a6d
tonystorey1 <tony.storey@bjss.com>
2025-05-27 11:31:48 +0100

    Remove corrupted files

--- README.md
+++
@@ -1,1 +0,0 @@
-# GoAcademyWeek3Exercise
--- consts/consts.go
+++
@@ -1,7 +0,0 @@
-package consts
-
-const FileName = "TodoList.csv"
-const PathSeparator = "/"
-const TodoStatusNotStarted = "not started"
-const TodoStatusStarted = "started"
-const TodoStatusCompleted = "completed"
